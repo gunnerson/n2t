c713f3bd0bb6a4d76c90aad29cb51e710fde6be2
bwaargh <rumatallc@gmail.com>
2025-04-05 20:51:24 -0500

    Update 2025-04-05

--- src/test.asm
+++ src/test.asm
@@ -1,53 +1,41 @@
-// push constant 7
-@7
-D=A		// D = 7
+// push constant 16
+@16
+D=A		// D = 16
 @SP
 A=M
 M=D		// *SP = D
 @SP
 M=M+1		// SP++
-// push local 0
+// push temp 0
 @0
 D=A		// D = 0
-@LCL
-A=D+M		// D += LCL
-D=M		// D = *(LCL + D)
+@5
+A=D+A
+D=M		// D = *(D + 5)
 @SP
 A=M
 M=D		// *SP = D
 @SP
 M=M+1		// SP++
-// add
-@SP
-M=M-1		// SP--
-A=M
-D=M		// D = *SP
-@SP
-M=M-1		// SP--
-A=M
-M=D+M		// *SP += D
-@SP
-M=M+1		// SP++
-// pop local 1
+// push temp 1
 @1
 D=A		// D = 1
-@LCL
-D=D+M		// D += LCL
-@R13
-M=D		// R13 = D
-@SP
-M=M-1		// SP--
-@SP
-A=M
-D=M		// D = *SP
-@R13
-A=M
-M=D		// *R13 = D
-// push constant 42
-@42
-D=A		// D = 42
+@5
+A=D+A
+D=M		// D = *(D + 5)
 @SP
 A=M
 M=D		// *SP = D
+@SP
+M=M+1		// SP++
+// sub
+@SP
+M=M-1		// SP--
+A=M
+D=M		// D = *SP
+@SP
+M=M-1		// SP--
+A=M
+M=M-D		// *SP -= D
 @SP
 M=M+1		// SP++
--- src/translator.c
+++ src/translator.c
@@ -11,91 +11,128 @@
 #define MAX_TOKEN_LENGTH 16
 #define MAX_CONSTANT 32767
 
-// push_segment {{{1
-int push_segment(char *arg1, char *arg2) {
-  int c = atoi(arg2);
-  if (c < 0 || c > MAX_CONSTANT) {
-    perror("[!] Address out of bounds!");
-    return EXIT_FAILURE;
-  }
-
-  if (!strcmp(arg1, "constant")) {
-    printf("@%d\n", c);
-    printf("D=A\t\t// D = %d\n", c);
-  } else if (!strcmp(arg1, "argument")) {
-
-  } else if (!strcmp(arg1, "local")) {
-    printf("@%d\n", c);
-    printf("D=A\t\t// D = %d\n", c);
-    printf("@LCL\n");
-    printf("A=D+M\t\t// D += LCL\n");
-    printf("D=M\t\t// D = *(LCL + D)\n");
-
-  } else if (!strcmp(arg1, "static")) {
-  } else if (!strcmp(arg1, "this")) {
-  } else if (!strcmp(arg1, "that")) {
-  } else if (!strcmp(arg1, "pointer")) {
-  } else if (!strcmp(arg1, "temp")) {
-
-  } else {
-    perror("[!] Invalid segment!");
-    return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
-}
-
-// pop_segment {{{1
-int pop_segment(char *arg1, char *arg2) {
-  int c = atoi(arg2);
-  if (c < 0 || c > MAX_CONSTANT) {
-    perror("[!] Address out of bounds!");
-    return EXIT_FAILURE;
-  }
-
-  if (!strcmp(arg1, "local")) {
-    printf("@%d\n", c);
-    printf("D=A\t\t// D = %d\n", c);
-    printf("@LCL\n");
-    printf("D=D+M\t\t// D += LCL\n");
-    printf("@R13\n");
-    printf("M=D\t\t// R13 = D\n");
-
-  } else if (!strcmp(arg1, "static")) {
-  } else if (!strcmp(arg1, "this")) {
-  } else if (!strcmp(arg1, "that")) {
-  } else if (!strcmp(arg1, "pointer")) {
-  } else if (!strcmp(arg1, "temp")) {
-
-  } else {
-    perror("[!] Invalid segment!");
-    return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
-}
-
 // print_command {{{1
-int print_command(char *command, char *arg1, char *arg2) {
+int print_command(char *command, char *arg1, char *arg2, char *file_name) {
+  int c = atoi(arg2);
+  if (c < 0 || c > MAX_CONSTANT) {
+    perror("[!] Address out of bounds!");
+    return EXIT_FAILURE;
+  }
+
+  // push {{{2
   if (!strcmp(command, "push")) {
-    if (push_segment(arg1, arg2))
+
+    // constant {{{3
+    if (!strcmp(arg1, "constant")) {
+      printf("@%d\n", c);
+      printf("D=A\t\t// D = %d\n", c);
+
+      // local {{{3
+    } else if (!strcmp(arg1, "local")) {
+      printf("@%d\n", c);
+      printf("D=A\t\t// D = %d\n", c);
+      printf("@LCL\n");
+      printf("A=D+M\t\t// D += LCL\n");
+      printf("D=M\t\t// D = *(LCL + D)\n");
+
+      // static {{{3
+    } else if (!strcmp(arg1, "static")) {
+      printf("@%s.%d\n", file_name, c);
+      printf("D=M\t\t// D = *%s.%d\n", file_name, c);
+
+      // pointer {{{3
+    } else if (!strcmp(arg1, "pointer")) {
+      if (c) {
+        printf("@THAT\n");
+      } else
+        printf("@THIS\n");
+      printf("D=M\t\t// D = *(THIS/THAT)\n");
+
+      // temp {{{3
+    } else if (!strcmp(arg1, "temp")) {
+      printf("@%d\n", c);
+      printf("D=A\t\t// D = %d\n", c);
+      printf("@5\n");
+      printf("A=D+A\n");
+      printf("D=M\t\t// D = *(D + 5)\n");
+      // }}}3
+
+    } else {
+      perror("[!] Invalid segment!");
       return EXIT_FAILURE;
+    }
     printf("@SP\n");
     printf("A=M\n");
     printf("M=D\t\t// *SP = D\n");
     printf("@SP\n");
     printf("M=M+1\t\t// SP++\n");
 
+    // pop {{{2
   } else if (!strcmp(command, "pop")) {
-    if (pop_segment(arg1, arg2))
-      return EXIT_FAILURE;
-    printf("@SP\n");
-    printf("M=M-1\t\t// SP--\n");
-    printf("@SP\n");
-    printf("A=M\n");
-    printf("D=M\t\t// D = *SP\n");
-    printf("@R13\n");
-    printf("A=M\n");
-    printf("M=D\t\t// *R13 = D\n");
 
+    // static {{{3
+    if (!strcmp(arg1, "static")) {
+      printf("@SP\n");
+      printf("M=M-1\t\t// SP--\n");
+      printf("A=M\n");
+      printf("D=M\t\t// D = *SP\n");
+      printf("@%s.%s\n", file_name, arg2);
+      printf("M=D\t\t// *%s.%s = D\n", file_name, arg2);
+
+      // local {{{3
+    } else if (!strcmp(arg1, "local")) {
+      printf("@%d\n", c);
+      printf("D=A\t\t// D = %d\n", c);
+      printf("@LCL\n");
+      printf("D=D+M\t\t// D += LCL\n");
+      printf("@R13\n");
+      printf("M=D\t\t// R13 = D\n");
+      printf("@SP\n");
+      printf("M=M-1\t\t// SP--\n");
+      printf("@SP\n");
+      printf("A=M\n");
+      printf("D=M\t\t// D = *SP\n");
+      printf("@R13\n");
+      printf("A=M\n");
+      printf("M=D\t\t// *R13 = D\n");
+
+      // pointer {{{3
+    } else if (!strcmp(arg1, "pointer")) {
+      printf("@SP\n");
+      printf("M=M-1\t\t// SP--\n");
+      printf("@SP\n");
+      printf("A=M\n");
+      printf("D=M\t\t// D = *SP\n");
+      if (c) {
+        printf("@THAT\n");
+      } else
+        printf("@THIS\n");
+      printf("M=D\t\t// *(THIS/THAT) = D\n");
+
+      // temp {{{3
+    } else if (!strcmp(arg1, "temp")) {
+      printf("@%d\n", c);
+      printf("D=A\t\t// D = %d\n", c);
+      printf("@5\n");
+      printf("D=D+A\t\t// D += 5\n");
+      printf("@R13\n");
+      printf("M=D\t\t// R13 = D\n");
+      printf("@SP\n");
+      printf("M=M-1\t\t// SP--\n");
+      printf("@SP\n");
+      printf("A=M\n");
+      printf("D=M\t\t// D = *SP\n");
+      printf("@R13\n");
+      printf("A=M\n");
+      printf("M=D\t\t// *R13 = D\n");
+      // }}}3
+
+    } else {
+      perror("[!] Invalid segment!");
+      return EXIT_FAILURE;
+    }
+
+    // add {{{2
   } else if (!strcmp(command, "add")) {
     printf("@SP\n");
     printf("M=M-1\t\t// SP--\n");
@@ -108,13 +145,31 @@ int print_command(char *command, char *arg1, char *arg2) {
     printf("@SP\n");
     printf("M=M+1\t\t// SP++\n");
 
+    // sub {{{2
   } else if (!strcmp(command, "sub")) {
+    printf("@SP\n");
+    printf("M=M-1\t\t// SP--\n");
+    printf("A=M\n");
+    printf("D=M\t\t// D = *SP\n");
+    printf("@SP\n");
+    printf("M=M-1\t\t// SP--\n");
+    printf("A=M\n");
+    printf("M=M-D\t\t// *SP -= D\n");
+    printf("@SP\n");
+    printf("M=M+1\t\t// SP++\n");
+
   } else if (!strcmp(command, "neg")) {
+
   } else if (!strcmp(command, "eq")) {
+
   } else if (!strcmp(command, "gt")) {
+
   } else if (!strcmp(command, "lt")) {
+
   } else if (!strcmp(command, "and")) {
+
   } else if (!strcmp(command, "or")) {
+
   } else if (!strcmp(command, "not")) {
   }
 
@@ -125,11 +180,15 @@ int print_command(char *command, char *arg1, char *arg2) {
 int main(int argc, char *argv[]) {
   // handle file {{{2
   FILE *file;
+  char file_name[MAX_LINE_LENGTH] = {0};
   if (argc >= 2) {
     file = fopen(argv[1], "r");
     if (file == NULL) {
       perror("Error opening file");
       return EXIT_FAILURE;
+    }
+    for (size_t i = 0; argv[1][i] != '.'; i++) {
+      file_name[i] = argv[1][i];
     }
   } else {
     file = tmpfile();
@@ -143,6 +202,7 @@ int main(int argc, char *argv[]) {
       fwrite(buffer, 1, bytes_read, file);
     }
     rewind(file);
+    strcpy(file_name, "tmp");
   }
 
   // parser {{{2
@@ -187,8 +247,7 @@ int main(int argc, char *argv[]) {
         printf(" %s", arg2);
       printf("\n");
     }
-    if (print_command(command, arg1, arg2)) {
-      perror("[!] Translation error!");
+    if (print_command(command, arg1, arg2, file_name)) {
       fclose(file);
       return EXIT_FAILURE;
     }
